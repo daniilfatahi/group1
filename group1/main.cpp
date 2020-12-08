f48c36008a2c429daec631db428442988d473401
daniilfatahi <75694630+daniilfatahi@users.noreply.github.com>
2020-12-08 20:41:04 +0200

    Add files via upload

---
+++ group1/main.cpp
@@ -0,0 +1,86 @@
+//
+//  main.cpp
+//  group1
+//
+//  Created by cashmachine on 08.12.2020.
+//
+
+#include <iostream>
+#include <cmath>
+
+//   ЗАДАЧА №1
+
+
+//    int main () {
+//        double a,b,c;
+//       std::cout<<"Введите три числа:\n";
+//       std::cin>>a>>b>>c;
+//       std::cout<<"Среднее арифметическое: "<<(a+b+c)/3<<"\n";
+//        system("pause");
+//        return 0;
+//
+//    return 0;
+//}
+
+
+
+//ЗАДАЧА №2
+
+
+
+//int main() {
+//
+//
+//    std::cout<<"\n";
+//    float Enter_UA;
+//    std::cout<<"Enter UA:";
+//    std::cin>>Enter_UA;
+//    float UA_RUB_kurs=Enter_UA/0.38;
+//    std::cout<<"UA RUB kurs:"<<UA_RUB_kurs<<"\n";
+//    float UA_USD_kurs=Enter_UA/28.3;
+//    std::cout<<"UA USD kurs:"<<UA_USD_kurs<<"\n";
+//    float UA_EU_kurs=Enter_UA/33.5;
+//    std::cout<<"UA EU kurs:"<<UA_EU_kurs<<"\n";
+//
+//}
+
+
+//ЗАДАЧА №3
+
+
+
+//int main(){
+//    std::cout<<"To be or not to be"<<std::endl<<"/Shakespeare/";
+//}
+
+
+//ЗАДАЧА №4
+
+//int main() {
+//
+//    int n,a;
+//        std::cin>>n>>a;
+//        int nsave = n;
+//        if (a == 0) {std::cout<<1; return 0;}
+//        for(int i = 1; i < a; i++)
+//        {
+//            n*=nsave;
+//        }
+//        std::cout<<n;
+//
+//
+//}
+
+// ЗАДАЧА №5
+
+
+//int main(){
+//double a,b,c;
+//       std::cout<<"Введите три числа:\n";
+//       std::cin>>a>>b>>c;
+//       std::cout<<"Сумма: "<<(a+b+c)<<"\n";
+//        system("pause");
+//        return 0;
+//
+//    return 0;
+//}
